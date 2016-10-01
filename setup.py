756e6a19613217ee876f7f2924ecc8e40e9d9a52
Mike Dacre <mike.dacre@gmail.com>
2016-09-30 22:38:09 -0700

    Minor change to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='Anonymous Voter',
     version='0.1',
     long_description=__doc__,
-    packages=['annon_voter'],
+    packages=['annon_voter',
+              'annon_voter_site'],
     include_package_data=True,
     zip_safe=False,
     install_requires=['Flask']
